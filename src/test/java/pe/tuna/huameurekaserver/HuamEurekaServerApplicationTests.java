12c3abec89d6ffeec1ddc0458d9db2dfc3c0ce91
MiguDeveloper <miguel.peru.seo@gmail.com>
2020-11-24 17:37:08 -0500

    inicio proyecto

---
+++ src/test/java/pe/tuna/huameurekaserver/HuamEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.tuna.huameurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuamEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
